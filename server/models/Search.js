5d9d702412d3a54dfe814ba346cbb43a22aaf7b4
Ricardo Garcia <ricardougarcia@gmail.com>
2021-12-27 13:49:16 -0500

    made model file exportable and added commenting instructions

---
+++ server/models/Search.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const searchSchema = new Schema({
+  // TODO: add model object
+});
+
+const Search = model("Search", searchSchema);
+
+module.exports = Profile;
