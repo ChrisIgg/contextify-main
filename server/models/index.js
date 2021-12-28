c19f741f4807c0bf36826c53a820f7ccd5fee2ba
Ricardo Garcia <ricardougarcia@gmail.com>
2021-12-28 10:42:33 -0500

    added Item model blank file

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const Profile = require("./Profile");
 const Search = require("./Search");
+const Item = require("./Item");
 
-module.exports = { Profile, Search };
+module.exports = { Profile, Search, Item };
