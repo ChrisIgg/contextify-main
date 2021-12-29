a50901c61f023200b42a372e5e0077e5399f71a4
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2021-12-28 20:44:15 -0500

    created time page and added input field

--- client/src/pages/Time.js
+++ client/src/pages/Time.js
@@ -1,11 +1,52 @@
-import React from "react";
+import { React, useState } from "react";
+import { useQuery } from "@apollo/client";
 
-function Time() {
+import {
+  InputGroup,
+  Dropdown,
+  SplitButton,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+import { QUERY_PROFILES } from "../utils/queries";
+
+const Time = () => {
+  const { loading, data } = useQuery(QUERY_PROFILES);
+  const [time, setTime] = useState("minutes");
+
+  function hours() {
+    setTime("hours");
+  }
+  function minutes() {
+    setTime("minutes");
+  }
+  function seconds() {
+    setTime("seconds");
+  }
   return (
-    <div>
-      <h1>This is the time page</h1>
-    </div>
+    <main>
+      <>
+        <InputGroup className="mb-3">
+          <FormControl
+            type="number"
+            aria-label="Text input with dropdown button"
+          />
+          <SplitButton
+            variant="outline-secondary"
+            title={time}
+            id="segmented-button-dropdown-2"
+            alignRight
+          >
+            <Dropdown.Item onClick={hours}>hours</Dropdown.Item>
+            <Dropdown.Item onClick={minutes}>minutes</Dropdown.Item>
+            <Dropdown.Item onClick={seconds}>seconds</Dropdown.Item>
+          </SplitButton>
+          <Button variant="outline-info">Search</Button>{" "}
+        </InputGroup>
+      </>
+    </main>
   );
-}
+};
 
 export default Time;
