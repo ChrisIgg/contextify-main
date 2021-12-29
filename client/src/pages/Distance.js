e7f69bff673cfce2232675616d2a3cd4f0ff15e7
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2021-12-28 20:41:47 -0500

    created distance page and added input field

--- client/src/pages/Distance.js
+++ client/src/pages/Distance.js
@@ -1,10 +1,44 @@
-import React from "react";
-
+import { React, useState } from "react";
+import {
+  InputGroup,
+  Dropdown,
+  SplitButton,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 function Distance() {
+  const [distance, setDistance] = useState("ft");
+  function feet() {
+    setDistance("ft");
+  }
+  function inches() {
+    setDistance("in");
+  }
+  function centimeters() {
+    setDistance("cm");
+  }
   return (
-    <div>
-      <h1>This is the distance page</h1>
-    </div>
+    <main>
+      <>
+        <InputGroup className="mb-3">
+          <FormControl
+            type="number"
+            aria-label="Text input with dropdown button"
+          />
+          <SplitButton
+            variant="outline-secondary"
+            title={distance}
+            id="segmented-button-dropdown-2"
+            alignRight
+          >
+            <Dropdown.Item onClick={feet}>ft</Dropdown.Item>
+            <Dropdown.Item onClick={inches}>in</Dropdown.Item>
+            <Dropdown.Item onClick={centimeters}>cm</Dropdown.Item>
+          </SplitButton>
+          <Button variant="outline-info">Search</Button>{" "}
+        </InputGroup>
+      </>
+    </main>
   );
 }
 
