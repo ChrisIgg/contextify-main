212015f451926e9aa336e71c688b9b05960c68f2
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2021-12-28 20:32:30 -0500

    created the weight page and added user input

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,28 +1,51 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import { React, useState } from "react";
+import { useQuery } from "@apollo/client";
 
-import ProfileList from '../components/ProfileList';
+import {
+  InputGroup,
+  Dropdown,
+  SplitButton,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import ProfileList from "../components/ProfileList";
 
-import { QUERY_PROFILES } from '../utils/queries';
+import { QUERY_PROFILES } from "../utils/queries";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_PROFILES);
-  const profiles = data?.profiles || [];
+  const [inputType, setInputType] = useState("Lbs");
 
+  function pounds() {
+    setInputType("lbs");
+  }
+  function oz() {
+    setInputType("oz");
+  }
+  function grams() {
+    setInputType("g");
+  }
   return (
     <main>
-      <div className="flex-row justify-center">
-        <div className="col-12 col-md-10 my-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ProfileList
-              profiles={profiles}
-              title="Here's the current roster of friends..."
-            />
-          )}
-        </div>
-      </div>
+      <>
+        <InputGroup className="mb-3">
+          <FormControl
+            type="number"
+            aria-label="Text input with dropdown button"
+          />
+          <SplitButton
+            variant="outline-secondary"
+            title={inputType}
+            id="segmented-button-dropdown-2"
+            alignRight
+          >
+            <Dropdown.Item onClick={pounds}>lbs</Dropdown.Item>
+            <Dropdown.Item onClick={oz}>oz</Dropdown.Item>
+            <Dropdown.Item onClick={grams}>g</Dropdown.Item>
+          </SplitButton>
+          <Button variant="outline-info">Search</Button>{" "}
+        </InputGroup>
+      </>
     </main>
   );
 };
