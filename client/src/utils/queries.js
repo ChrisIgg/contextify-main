65f02e24d065db91a698f1dc41e18763726220d1
Ami Asokumar <gasokumar@gmail.com>
2022-01-02 00:29:52 -0500

    added comments and started changing queries.js to match typeDefs.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,16 +1,28 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+// Theres should be four queries in this file: profiles, profile, me, and searches.
+// Look to typeDefs in server --> schemas --> typeDefs to verify.
 
+//
 export const QUERY_PROFILES = gql`
   query allProfiles {
+    # This is the query
+    # Name of query, and then everything it should return.
+    # The allprofiles query returns an array of profiles,
     profiles {
       _id
       name
+      # We probably don't want skills to be returned for our users, instead we'll probably want a list of their saved searches?
       skills
     }
   }
 `;
 
 export const QUERY_SINGLE_PROFILE = gql`
+# query (queryName($What it takes in: What it returns)) {
+# queryName from typeDef(name)
+# }
+
+}
   query singleProfile($profileId: ID!) {
     profile(profileId: $profileId) {
       _id
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,9 @@
 const { gql } = require("apollo-server-express");
+// Typedefs in this file: Profile, Auth, Search, and Items.
 
+// Queries in this file: profiles, profile, me, and searches
+
+// Mutations in this file: addProfile, login, addSearch, removeProfile, removeSkill
 const typeDefs = gql`
   type Profile {
     _id: ID
@@ -32,16 +36,22 @@ const typeDefs = gql`
   }
 
   type Query {
+    # The profiles query returns an array of Profiles
     profiles: [Profile]!
+    # The profile query takes in a profileId and returns a profile
     profile(profileId: ID!): Profile
     # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
     me: Profile
+    # searches
     searches: [Search]
   }
 
   type Mutation {
+    # addProfile takes in a name, email and password, returns Auth
     addProfile(name: String!, email: String!, password: String!): Auth
+    # Login takes in an email and password, returns Auth
     login(email: String!, password: String!): Auth
+    # addSearch takes in a searchId, name, weight, mass, diameter, distance, and duration and return a Search
     addSearch(
       searchId: ID!
       name: String!
@@ -51,7 +61,9 @@ const typeDefs = gql`
       distance: number
       duration: number
     ): Search
+    # removeProfile
     removeProfile: Profile
+    # removeSkill takes in a skill, returns a profile.
     removeSkill(skill: String!): Profile
   }
 `;
