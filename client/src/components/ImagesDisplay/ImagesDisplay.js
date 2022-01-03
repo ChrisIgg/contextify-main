065d58f7706c0cc87b67c287c468d52fc0a89371
Chris Insignares <insignaresc115@gmail.com>
2022-01-03 15:42:03 -0500

    add ranges and sizes for images that are displayed

--- client/src/components/ImagesDisplay/ImagesDisplay.js
+++ client/src/components/ImagesDisplay/ImagesDisplay.js
@@ -1,20 +1,38 @@
 // import fonts
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAppleAlt } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faAppleAlt } from "@fortawesome/free-solid-svg-icons";
 // import Weight from '../../pages/Weight';
+import apple from "../../assets/apple.png";
 
 const ImageDisplay = ({ weight }) => {
-  // const newWeight = props.weight;
-  console.log(weight, "image display");
-
-  return (
-    <div>
-      {[...Array(Math.round(weight))].map(() => (
-        <FontAwesomeIcon icon={faAppleAlt} />
-      ))}
-      {/* {weight} */}
-    </div>
-  );
+  if (weight > 275 && weight < 799) {
+    return (
+      <div className="justify-content-center">
+        {[...Array(Math.round(weight))].map(() => (
+          <img src={apple} height="50" />
+        ))}
+        {/* {weight} */}
+      </div>
+    );
+  } else if (weight > 800) {
+    return (
+      <div className="justify-content-center">
+        {[...Array(Math.round(weight))].map(() => (
+          <img src={apple} height="25" />
+        ))}
+        {/* {weight} */}
+      </div>
+    );
+  } else {
+    return (
+      <div className="justify-content-center">
+        {[...Array(Math.round(weight))].map(() => (
+          <img src={apple} height="75" />
+        ))}
+        {/* {weight} */}
+      </div>
+    );
+  }
 };
 
 // const createBanana =
