990318c586f7bff39dd5989bcec5c31012aa2868
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2022-01-04 12:16:47 -0500

    created save search button

--- client/src/components/Output/Output.js
+++ client/src/components/Output/Output.js
@@ -1,16 +1,20 @@
-import React from "react";
-
-const output = ({ answer, output }) => {
+import { React } from "react";
+import { Button } from "react-bootstrap";
+import Auth from "../../utils/auth";
+import Confirm from "../Confirm/Confirm";
+const Output = ({ answer, output }) => {
+  // const [show, setShow] = useState(true);
   if (answer) {
     return (
       <div>
         <h1>
           {Math.round(answer)} {output}
         </h1>
+        <Confirm />
       </div>
     );
   }
   return <div></div>;
 };
 
-export default output;
+export default Output;
