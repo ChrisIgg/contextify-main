5696c6eda3542baf9f824bb2bf249bece0e55e1a
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2022-01-04 13:32:52 -0500

    fixed error in output.js

--- client/src/components/Output/Output.js
+++ client/src/components/Output/Output.js
@@ -1,14 +1,12 @@
 import { React } from "react";
 
 import Confirm from "../Confirm/Confirm";
-const Output = ({ answer, output }) => {
-import React from "react";
 const styles = {
   font: {
     fontFamily: "Ubuntu, sans-serif",
   },
 };
-const output = ({ answer, output }) => {
+const Output = ({ answer, output }) => {
   if (answer) {
     return (
       <div style={styles.font}>
